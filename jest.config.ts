9f4dd61818fbb9563a74112b77c226f06f95dc5b
Pcrab <pcrab@pcrab.xyz>
2023-04-05 17:10:34 +0800

    feat: use swc jest to speed up test

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,9 @@
 import type { Config } from "jest";
 
 const config: Config = {
-    preset: "ts-jest/presets/default-esm",
-    moduleNameMapper: {
-        "^(\\.{1,2}/.*)\\.js$": "$1",
-    },
-    testMatch: ["**/__tests__/**/*.+(ts)", "**/?(*.)+(spec|test).+(ts)"],
     collectCoverage: true,
     transform: {
-        "<regex_match_files": [
-            "ts-jest",
-            {
-                useESM: true,
-            },
-        ],
+        "^.+\\.(t|j)sx?$": "@swc/jest",
     },
 };
 
